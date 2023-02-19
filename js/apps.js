f814143fe87288b0af2fcf4af31ff9b652a7f5e8
rhsabbir1 <rhsabbir090@gmai.com>
2023-02-19 22:24:42 +0600

    new friend add by js

--- js/apps.js
+++ js/apps.js
@@ -1,16 +1,32 @@
 
 // background Color
-document.getElementById('bcg-colour').addEventListener('click', function(){
+document.getElementById('bcg-colour').addEventListener('click', function () {
     const friends = document.getElementsByClassName('friend');
 
-    for(const friend of friends){
+    for (const friend of friends) {
         friend.style.backgroundColor = 'indianred'
         console.log(friend)
     }
 })
 
 // text align center 
-document.getElementById("text-center").addEventListener('click' , function(){
+document.getElementById("text-center").addEventListener('click', function () {
     const thirdFriend = document.getElementById('third-friend')
     thirdFriend.style.textAlign = 'center';
+})
+
+
+// add-new-firend
+document.getElementById('add-new-firend').addEventListener('click', function () {
+    const friendContainer = document.getElementById('friend-container');
+
+    const newElement = document.createElement('div');
+    newElement.classList.add('friend')
+    newElement.innerHTML = `
+    <div">
+        <h1>Firend</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem, deleniti?</p>
+    </div>
+    `
+    friendContainer.appendChild(newElement)
 })
