610506d69d368cac60ff4a453e0cc940f9d4c5f5
rhsabbir1 <rhsabbir090@gmai.com>
2023-02-19 23:17:14 +0600

    use backtick

---
+++ js/template.js
@@ -0,0 +1,24 @@
+const friendName = 'Moriom';
+const bestFrnd = 'Moriom';
+const lastFriend =`Moriom`;
+
+
+const multiLine = 'This is moriom \n' +
+'she is my best friend \n' + 
+'She is one for the best frnd \n';
+
+// console.log(multiLine)
+
+const backticMultiline = `This is moriom
+she is my best friend
+She is one for the best friend 
+`;
+// console.log(backticMultiline)
+const a = 10;
+const b =12;
+
+const summary = 'sum of : ' + a + ' and ' + b + ' is ' + (a + b);
+// console.log(summary)
+
+const newSummary = `sum of : ${a} and ${b} is ${a+b}`;
+console.log(newSummary)
