00684b30bf0d277a3bf95b55669144008a713219
rhsabbir1 <rhsabbir090@gmai.com>
2023-02-19 22:15:19 +0600

    text align center by id

--- js/apps.js
+++ js/apps.js
@@ -1,3 +1,5 @@
+
+// background Color
 document.getElementById('bcg-colour').addEventListener('click', function(){
     const friends = document.getElementsByClassName('friend');
 
@@ -5,4 +7,10 @@ document.getElementById('bcg-colour').addEventListener('click', function(){
         friend.style.backgroundColor = 'indianred'
         console.log(friend)
     }
+})
+
+// text align center 
+document.getElementById("text-center").addEventListener('click' , function(){
+    const thirdFriend = document.getElementById('third-friend')
+    thirdFriend.style.textAlign = 'center';
 })
