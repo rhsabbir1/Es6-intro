cfab9c10604be4bd35c7ce9c2a959c915db9a28c
rhsabbir1 <rhsabbir090@gmai.com>
2023-02-20 15:38:55 +0600

    arrow function

---
+++ js/arrow.js
@@ -0,0 +1,14 @@
+function add(first , first){
+    const total = first + first;
+    return total;
+}
+
+const numberAdd = add(5 , 4)
+console.log()
+
+
+const sum = (first , second) =>{
+    console.log(first + second)
+}
+sum(5, 4)
+
